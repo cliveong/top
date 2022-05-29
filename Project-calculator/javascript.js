eab131ee58722b2e77119175d7c93726e6c64d54
Clive <cliveong1999@gmail.com>
2022-05-30 02:33:47 +0800

    Finish Project calc

---
+++ Project-calculator/javascript.js
@@ -0,0 +1,191 @@
+let numberOne = "";
+let numberTwo = "";
+let switchNum = false;
+let operator = null;
+let overrideOperator = false;
+
+const numbers = document.querySelectorAll(".num");
+const oper = document.querySelectorAll(".oper");
+const clear = document.querySelector(".clear");
+const point = document.querySelector(".point");
+const equal = document.querySelector(".equal");
+const display = document.querySelector(".screen");
+const negative = document.querySelector(".negative");
+
+numbers.forEach((button) =>
+    button.addEventListener("click", () => {
+        addToNumber(button.textContent);
+    }));
+
+function addToNumber(num) {
+    if (error()) return;
+    overrideOperator = false;
+    if (!switchNum) {
+        numberOne = numberOne + num;
+        displayScreen(numberOne);
+    } else {
+        numberTwo = numberTwo + num;
+        displayScreen(numberTwo);
+    }
+}
+
+point.addEventListener("click", () => {
+    pointify();
+});
+
+function pointify() {
+    if (error()) return;
+    overrideOperator = false;
+    if (!switchNum) {
+        if (!(numberOne.includes("."))) {
+            numberOne = numberOne + ".";;
+            displayScreen(numberOne);
+        }    
+    } else {
+        if (!(numberTwo.includes("."))) {
+            numberTwo = numberTwo + ".";
+            displayScreen(numberTwo);
+        }    
+    }
+}
+
+oper.forEach((button) =>
+    button.addEventListener("click", () => {
+        operand(button.textContent);
+    }));
+    
+function operand(txt) {
+    if (error()) return;
+    if (numberTwo.length === 0) {
+        switchNum = true;
+    } else if (!(overrideOperator)){
+        let actlOne = Number(numberOne);
+        let actlTwo = Number(numberTwo);
+        if (operator === "+") {
+            addition(actlOne, actlTwo);
+        }
+        if (operator === "-") {
+            subtraction(actlOne, actlTwo);
+        }
+        if (operator === "x") {
+            multiply(actlOne, actlTwo);
+        }
+        if (operator === "÷") {
+            divide(actlOne, actlTwo);
+        }
+        displayScreen(numberOne);
+        
+    }
+    operator = txt;
+    overrideOperator = true;
+
+}
+
+function addition(a,b) {
+    try {
+        let num = a + b;
+        numberOne = num.toString();
+        numberTwo = "";
+    } catch (e) {
+        numberOne = "Error";
+        numberTwo = "";
+    }
+}
+
+function subtraction(a,b) {
+    try {
+        let num = a-b;
+        numberOne = num.toString();
+        numberTwo = "";
+    } catch (e) {
+        numberOne = "Error";
+        numberTwo = "";
+    }
+}
+
+function multiply(a,b) {
+    try {
+        let num = a*b;
+        numberOne = num.toString();
+        numberTwo = "";
+    } catch (e) {
+        numberOne = "Error";
+        numberTwo = "";
+    }
+}
+
+function divide(a,b) {
+    try {
+        if (b === 0) {
+            numberOne = "Error";
+            numberTwo = "";
+            return;
+        }
+        let num = a/b;
+        numberOne = num.toString();
+        numberTwo = "";
+    } catch (e) {
+        numberOne = "Error";
+        numberTwo = "";
+    }
+}
+
+clear.addEventListener("click", () => {
+    numberOne = "";
+    numberTwo = "";
+    switchNum = false;
+    operator = null;
+    overrideOperator = false;
+    display.textContent = null;
+});
+
+negative.addEventListener("click", () => {
+    if (error()) return;
+    overrideOperator = false;
+    if (!switchNum) {
+        if (!(numberOne.includes("-"))) {
+            numberOne ="-" + numberOne ;
+            
+        } else {
+            numberOne = numberOne.substring(1);
+        }
+        displayScreen(numberOne);
+    } else {
+        if (!(numberTwo.includes("-"))) {
+            numberTwo = "-" + numberTwo;         
+        } else {
+            numberTwo = numberTwo.substring(1);
+        }
+        displayScreen(numberTwo);
+    }
+});
+
+equal.addEventListener("click", () => {
+    if (error()) return;
+    operand(null);
+    overrideOperator = false;
+});
+
+function error() {
+    if (numberOne === "Error" || numberOne === "Overflow Error" || numberTwo === "Overflow Error") {
+        return true;
+    }
+    return false;
+}
+
+function displayScreen(number) {
+    if (Number(number) > 99999999999) {
+        number = "Overflow Error"
+    }
+    if (number.length > 11) {
+        number = number.substring(0,11)
+    }
+    display.textContent = number
+
+}
+
+
+
+
+
+
