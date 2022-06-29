0b2c846fd428be928b718e81faa06fe56b945c4a
Clive <cliveong1999@gmail.com>
2022-06-29 22:21:13 +0800

    Bug fix on logic to find winner

--- Project-tic-tac-toe/javascript.js
+++ Project-tic-tac-toe/javascript.js
@@ -1,6 +1,8 @@
 const gameBoard = () => {
     let firstPlayerTurn = true;
+    let firstPlayerWinner = null;
     let gameState = [0,0,0,0,0,0,0,0,0];
+    let winningCombinations
 
     const getTurn = () => firstPlayerTurn;
     const changeTurn = () => {
@@ -21,12 +23,15 @@ const gameBoard = () => {
                         updateGameState(cell ,2);
                     }
                     changeTurn();
+                    let winningArray = getWinner();
+                    highlightWinner(winningArray);
                 }
             }));
     };
 
+    //add cross or circle on given table cell
     const addCrossOrCircle = (cell, turn) => {
-        let cellWidth = (cell.offsetWidth-50).toString();
+        let cellWidth = (cell.offsetWidth-120).toString();
         if (turn === 1) {
             let cross = document.createElement("IMG");
             cross.src = "./images/close-trimmy.png";
@@ -40,44 +45,58 @@ const gameBoard = () => {
         }
     }
 
+    //update array of placed X and Os
     const updateGameState = (cell, turn) => {
         let index = parseInt((cell.className).slice(-1)-1);
         gameState[index] = turn;
-        console.log(gameState);
     }
 
+    //find the winner based on placed pieces (in game state)
     const getWinner = () => {
         //0 1 2 
         //3 4 5
         //6 7 8
-        if (
-            (gameState[0] === gameState[1] === gameState[2] && gameState[0] === 1) ||
-            (gameState[3] === gameState[4] === gameState[5] && gameState[0] === 1) ||
-            (gameState[6] === gameState[7] === gameState[8] && gameState[0] === 1) ||
-            (gameState[0] === gameState[3] === gameState[6] && gameState[0] === 1) ||
-            (gameState[1] === gameState[4] === gameState[7] && gameState[0] === 1) ||
-            (gameState[2] === gameState[5] === gameState[8] && gameState[0] === 1) ||
-            (gameState[0] === gameState[4] === gameState[8] && gameState[0] === 1) ||
-            (gameState[2] === gameState[4] === gameState[6] && gameState[0] === 1)
-        ) {
-            return 0;
-        } else if (
-            (gameState[0] === gameState[1] === gameState[2] && gameState[0] === 2) ||
-            (gameState[3] === gameState[4] === gameState[5] && gameState[0] === 2) ||
-            (gameState[6] === gameState[7] === gameState[8] && gameState[0] === 2) ||
-            (gameState[0] === gameState[3] === gameState[6] && gameState[0] === 2) ||
-            (gameState[1] === gameState[4] === gameState[7] && gameState[0] === 2) ||
-            (gameState[2] === gameState[5] === gameState[8] && gameState[0] === 2) ||
-            (gameState[0] === gameState[4] === gameState[8] && gameState[0] === 2) ||
-            (gameState[2] === gameState[4] === gameState[6] && gameState[0] === 2)
-        ) {
-            
+        const row1 = [0,1,2];
+        const row2 = [3,4,5];
+        const row3 = [6,7,8];
+        const col1 = [0,3,6];
+        const col2 = [1,4,7];
+        const col3 = [2,5,8];
+        const diag1 = [0,4,8];
+        const diag2 = [2,4,6];
+        const winningCombo = [row1, row2, row3, col1, col2, col3, diag1, diag2];
+        for(let i = 0; i < 8; i++) {
+            if (gameState[winningCombo[i][0]] === 
+                gameState[winningCombo[i][1]] &&
+                gameState[winningCombo[i][1]] === 
+                gameState[winningCombo[i][2]]) {
+                    if (gameState[winningCombo[i][0]] === 1) {
+                        // console.log(gameState[winningCombo[i][0]]);
+                        // console.log(gameState[winningCombo[i][1]]);
+                        // console.log(gameState[winningCombo[i][2]]);
+                        // console.log(winningCombo[i]);
+                        firstPlayerWinner = true;
+                        return winningCombo[i];
+                    } else if (gameState[winningCombo[i][0]] === 2){
+                        // console.log(gameState[winningCombo[i][0]]);
+                        // console.log(gameState[winningCombo[i][1]]);
+                        // console.log(gameState[winningCombo[i][2]]);
+                        // console.log(winningCombo[i]);
+                        firstPlayerWinner = false;
+                        return winningCombo[i];
+                    }                 
+                }
         }
+        firstPlayerWinner = null;
+        return null;
+    }
+
+    const highlightWinner = (winningArray) => {
+        let tempIndex = 0;
     }
 
     return {
-        startGame,
-        gameState
+        startGame, gameState
     };
 }
 
