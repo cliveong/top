71f0a7b5725c57fccc632fed4db9b2614abdfa88
Clive <cliveong1999@gmail.com>
2022-07-05 18:32:04 +0800

    Finish export/import tutorial

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 
+// import your function
+import myName from './myName';
+
 function component() {
-    const element = document.createElement('div');
-  
-  // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(component());
