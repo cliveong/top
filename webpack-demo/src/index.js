8dbf4c5cf93d177bb8aa58f4128f635ff81630da
Clive <cliveong1999@gmail.com>
2022-07-06 18:46:31 +0800

    finish webpack tut part 2

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,23 @@
 import _ from 'lodash';
-
+import './style.css';
 // import your function
 import myName from './myName';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   // use your function!
-  element.textContent = myName('Cody');
+  //element.textContent = myName('Cody');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 }
 
